18abc6721e0ca4896cb777bd9e3914b4189373dd
Arne Gevaert <arnegevaert@gmail.com>
2017-10-26 16:06:03 +0200

    Get interval

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-const dq = require('smartflanders-data-query');
+const sfquery = require('smartflanders-data-query');
 const http = require('http');
 const url = require('url');
+const querystring = require('querystring');
+const moment = require('moment');
 
 let port = process.env.NODE_PORT;
 if (!port) {
     console.error("No port specified! Usage: NODE_PORT=[PORT] pm2 start index.js");
     process.exit(1);
 } else {
-    console.log("Server running at port ", port);
+    console.log("Server running at port", port);
 }
 
 http.createServer(function (req, res) {
     let path = url.parse(req.url).pathname;
-    let query = url.parse(req.url).query;
     switch (path) {
-        case '/':
-            res.write('ROOT');
-            if (query) {
-                res.write(query);
-            }
+        case '/interval':
+            getInterval(req, res);
+            break;
+        case 'parkings':
+            getParkings(req, res);
             break;
         default:
             res.writeHead(404);
-            res.write('NOT GOOD');
+            res.write('Page not found.');
             break;
     }
-    res.end();
-}).listen(8000);
-console.log('Server running at localhost:8000');
+}).listen(port);
+
+const cities = {
+    'Gent': 'https://linked.open.gent/parking/',
+    'Kortrijk': 'https://kortrijk.datapiloten.be/parking',
+    'Sint-Niklaas': 'https://sint-niklaas.datapiloten.be/parking',
+    'Leuven': 'https://leuven.datapiloten.be/parking',
+    'Nederland': 'https://nederland.datapiloten.be/parking'
+};
+
+function getInterval(req, res) {
+    let query = url.parse(req.url).query;
+    let parsed = querystring.parse(query);
+
+    if (parsed.from && parsed.to && parsed.city) {
+        let from = moment(parsed.from).unix();
+        let to = moment(parsed.to).unix();
+        let dataset = cities[parsed.city];
+        if (from && to && dataset) {
+            let dq = new sfquery();
+            dq.addDataset(dataset);
+            let data = [];
+            dq.getInterval(from, to).subscribe(
+                d => {data.push(d)},
+                e => {},
+                () => {
+                    res.write(JSON.stringify(data));
+                    res.end();
+                }
+            );
+        } else {
+            res.writeHead(400);
+            res.write('Malformed arguments');
+            res.end();
+        }
+    } else {
+        res.writeHead(400);
+        res.write('Malformed request: Provide at least from, to and city.');
+        res.end();
+    }
+}
+
+function getParkings(req, res) {
+
+}
